b1150cdb7508bcd3cbfe11e3afc4eca5cde511af
Sebastian Robles Camacho <sebastian.rob.cam@gmail.com>
2024-04-01 22:09:15 -0600

    feat: Function to take the user's parameters to create a set of numbers.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use text_io::read;
+
+fn create_set_numbers() -> Vec<i32> {
+    print!("Write the first number of your set: ");
+    let start_number: i32 = read!();
+
+    print!("Write the last number of your set: ");
+    let end_number: i32 = read!();
+
+    let mut set_of_numbers: Vec<i32> = Vec::new();
+
+    for i in start_number..(end_number + 1) {
+        set_of_numbers.push(i);
+    }
+
+    set_of_numbers
+}
+
 fn main() {
     println!("Hello, world!");
 }
